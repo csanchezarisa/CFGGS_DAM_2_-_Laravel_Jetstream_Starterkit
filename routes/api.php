19c87386fd791c8c674a3f72468bd7407ddcdfd7
ulkiobal <ulkiobal@users.noreply.github.com>
2021-03-04 19:44:19 +0100

    Ruta para probar conexión con la api

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,10 @@ use App\Http\Controllers\ApioController;
 |
 */
 
+Route::get('/prueba', function() {
+    return response('ok', 200, array());
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
